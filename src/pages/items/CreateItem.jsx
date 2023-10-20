60eaf95b95e9525eb36c21d5cf1a18727a545893
Ronaldo Arley <ronaldmamani140@gmail.com>
2023-10-20 20:09:50 -0300

    Adicionando a seção Items e colocando rotas filhas nessa seção

---
+++ src/pages/items/CreateItem.jsx
@@ -0,0 +1,7 @@
+export default function CreateItem() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
--- src/router.jsx
+++ src/router.jsx
@@ -1,18 +1,25 @@
 import { createBrowserRouter } from "react-router-dom";
+import Home from "./pages/Home";
+import CreateItem from "./pages/items/CreateItem";
+import ShowItem from "./pages/items/ShowItem";
+import UpdateItem from "./pages/items/UpdateItem";
+import ListItems from "./pages/items/ListItems";
+import ItemsLayout from "./pages/items/Layout";
+import RootLayout from "./pages/RootLayout";
 
 const router = createBrowserRouter([{
     path: "/",
-
+    element: <RootLayout />,
     children: [
-        { index: true, },
+        { index: true, element: <Home />},
         {
             path: "items",
-            
+            element: <ItemsLayout />,
             children: [
-                { index: true, },
-                { path: "new", },
-                { path: ":id", },
-                { path: ":id/update", },
+                { index: true, element: <ListItems />},
+                { path: "new", element: <CreateItem />},
+                { path: ":id", element: <ShowItem />},
+                { path: ":id/update", element: <UpdateItem />},
             ]
         }
     ]
