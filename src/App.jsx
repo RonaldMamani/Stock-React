8fa13585f98e198753754f160f718d1f1bea6910
Ronaldo Arley <ronaldmamani140@gmail.com>
2023-10-23 19:47:10 -0300

    Adicionando Lista de Itens do estoque

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { RouterProvider } from 'react-router-dom'
 import router from './router'
+import { StockContext, StockContextProvider } from './contexts/StockContext'
 
 function App() {
 
   return (
-    <>
+    <StockContextProvider>
       <RouterProvider router={router} />
-    </>
+    </StockContextProvider>
   )
 }
 
--- src/pages/items/ListItems.jsx
+++ src/pages/items/ListItems.jsx
@@ -1,7 +1,10 @@
+import ItemsTable from "../../components/ItemsTable";
+
 export default function ListItems() {
     return (
         <div>
             <h3>Todos os Itens</h3>
+            <ItemsTable />
         </div>
     )
 }
